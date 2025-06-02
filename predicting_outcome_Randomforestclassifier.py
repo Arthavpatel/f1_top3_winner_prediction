b6257dd52b8e32abac0f7d166be92162c9b1f1e6
Arthav Patel <arthavpatel@Arthavs-MacBook-Pro-6.local>
2025-06-01 21:47:09 -0700

    calculating and storing avgerage degradation by different compounds in FP1, FP2 and FP3

--- predicting_outcome_Randomforestclassifier.py
+++ predicting_outcome_Randomforestclassifier.py
@@ -80,3 +80,64 @@ if laps_FP3 is not None:
             avg_time_driver[drv] = avg_time
         avg_time_by_compound_FP3[compound] = avg_time_driver
 
+# ---------------------------------- Average Degradation for each compound ------------------------------------ #
+
+#Function to calculate the avg degradation 
+def avg_degradation(laps_df, driver, compound):
+    driver_laps = laps_df.pick_drivers([driver])
+    compound_laps = driver_laps[(driver_laps['Compound'] == compound) 
+                                & (driver_laps['IsAccurate'] == True)
+                                & (driver_laps['PitInTime'].isna()) 
+                                & (driver_laps['LapTime'].notna())]
+    if compound_laps.empty:
+        return None
+    degradation = []
+    stints = compound_laps.groupby('Stint')
+    for _, stint in stints:
+        laps_in_stint = stint.sort_values('LapNumber')
+        if len(laps_in_stint) < 3:
+            continue
+        first_lap = laps_in_stint['LapTime'].iloc[0].total_seconds()
+        last_lap = laps_in_stint['LapTime'].iloc[-1].total_seconds()
+        degr = (last_lap - first_lap) / (len(stint) - 1)
+        if degr < 0 or degr > 2:
+            continue
+        degradation.append(degr)
+    if degradation:
+        return np.mean(degradation)
+    else:
+        return None
+
+# Storing data in dictionary for FP1
+if laps_FP1 is not None:
+    compounds = laps_FP1['Compound'].unique()
+    avg_degradation_by_compound_FP1 = {}
+    for compound in compounds:
+        avg_degradation_driver = {}
+        for drv in laps_FP1['Driver'].unique():
+            avg_deg = avg_degradation(laps_FP1, drv, compound)
+            avg_degradation_driver[drv] = float(avg_deg) if avg_deg is not None else None
+        avg_degradation_by_compound_FP1[compound] = avg_degradation_driver
+
+# Storing data in dictionary for FP2
+if laps_FP2 is not None:
+    compounds = laps_FP2['Compound'].unique()
+    avg_degradation_by_compound_FP2 = {}
+    for compound in compounds:
+        avg_degradation_driver = {}
+        for drv in laps_FP2['Driver'].unique():
+            avg_deg = avg_degradation(laps_FP2, drv, compound)
+            avg_degradation_driver[drv] = float(avg_deg) if avg_deg is not None else None
+        avg_degradation_by_compound_FP2[compound] = avg_degradation_driver
+
+# Storing data in dictionary for FP3
+if laps_FP3 is not None:
+    compounds = laps_FP3['Compound'].unique()
+    avg_degradation_by_compound_FP3 = {}
+    for compound in compounds:
+        avg_degradation_driver = {}
+        for drv in laps_FP3['Driver'].unique():
+            avg_deg = avg_degradation(laps_FP3, drv, compound)
+            avg_degradation_driver[drv] = float(avg_deg) if avg_deg is not None else None
+        avg_degradation_by_compound_FP3[compound] = avg_degradation_driver
+pprint.pprint(avg_degradation_by_compound_FP3)
